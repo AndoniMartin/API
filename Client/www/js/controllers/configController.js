3b49bd56c08a1ca14cd0ff5b7d220386fbee61bc
0xJCG <jcastro004@ikasle.ehu.eus>
2015-03-14 13:02:14 +0100

    Controlador de la configuración terminado.

--- Client/www/js/controllers/configController.js
+++ Client/www/js/controllers/configController.js
@@ -7,29 +7,74 @@ angular.module('APINotack.configController', ['ionic'])
         	var newPass = config.newpass;
         	var repPass = config.reppass;
         	
-        	if (newPass == repPass) {
-        		/* Comprobar en local Storage la contraseña actual. */
-        		
-        	}
-        	
-        	/* Llamando al server. */
-        	var promise=restClient.changePass(contraseñaVieja,contraseñaNueva,contraseñaRep);
-        	promise.then(function(updated){
-        		if(updated) {
-        			var user=new Object();
-        			user.name=null;
-        			user.pass=null;
-        			user.logged=logged;
-        			localStorage.setItem("user",JSON.stringify(user));
-        		} else {
-				   var alertPopup = $ionicPopup.alert({
-				     title: 'Imposible actualizar contraseña',
-				     template: 'Las contraseñas no coinciden o la original es incorrecta.'
-				   });
-				   alertPopup.then(function(res) {
-				     ;
-				   });
+        	if (oldPass !== null && newPass !== null & repPass != null) { // Campos no vacíos.
+        		newPass = sha512(newPass);
+        		repPass = sha512(repPass);
+        		if (newPass == repPass) { // Los campos de la nueva contraseña coinciden.
+	        		/* Comprobar en local Storage la contraseña actual. */
+	        		oldPass = sha512(oldPass);
+	        		var savedPass = JSON.parse(localStorage.getItem("user")).pass;
+	        		if (oldPass == savedPass) {
+	        			var username = JSON.parse(localStorage.getItem("user")).name;
+	        			/* Llamando al server. */
+	                	var promise=restClient.changePass(username, oldPass, newPass);
+	                	promise.then(function(updated) {
+	                		if (updated) {
+	                			/* Guardar el usuario en el local storage. */
+	                			var user = new Object();
+	                			user.name = username;
+	                			user.pass = newPass;
+	                			user.logged = logged;
+	                			localStorage.setItem("user", JSON.stringify(user));
+	                			
+	                			/* Mostrar popUp. */
+	                			var alertPopup = $ionicPopup.alert({
+	                				title: 'Contraseña actualizada',
+	                				template: 'La contraseña ha sido correctamente actualizada.'
+	                				});
+	                			alertPopup.then(function(res) {
+	                				null;
+	                			});
+	                		} else {
+	                			/* Mostrar popUp. */
+	                			var alertPopup = $ionicPopup.alert({
+	                				title: 'Error',
+	                				template: 'No se ha podido actualizar la contraseña.'
+	                				});
+	                			alertPopup.then(function(res) {
+	                				null;
+	                			});
+	                		}
+	                	})
+	        		} else { // Contraseña antigua no coincide.
+	        			/* Mostrar popUp. */
+            			var alertPopup = $ionicPopup.alert({
+            				title: 'Las contraseñas no coinciden',
+            				template: 'Su antigua contraseña no coincide con la almacenada.'
+            				});
+            			alertPopup.then(function(res) {
+            				null;
+            			});
+	        		}
+        		} else { // Nuevas contraseñas no coinciden.
+        			/* Mostrar popUp. */
+        			var alertPopup = $ionicPopup.alert({
+        				title: 'Las contraseñas no coinciden',
+        				template: 'Los campos de la nueva contraseña no coinciden.'
+        				});
+        			alertPopup.then(function(res) {
+        				null;
+        			});
         		}
-        	})
+        	} else { // Algún campo vacío.
+        		/* Mostrar popUp. */
+    			var alertPopup = $ionicPopup.alert({
+    				title: 'Error',
+    				template: 'Existe algún campo no rellenado.'
+    				});
+    			alertPopup.then(function(res) {
+    				null;
+    			});
+        	}
         }
 })
