8a1b2a42322a8d346c7f34c06e28c53c4ec5818f
david <davidramirez_28@hotmail.com>
2015-03-14 12:11:05 +0100

    listo el changePass

--- Server/controllers/userController.js
+++ Server/controllers/userController.js
@@ -21,5 +21,27 @@ exports.login=function(request,response)
 
 exports.changePass=function(request,response)
 {
-	//TODO implementar
+	var b=request.body;
+	console.log(request);
+	
+	//Comprobar que la contraseña vieja del usuario es correcta
+	User.find({US_NAME:b.US_NAME},function(error,user){
+		if(error){
+			response.status(500).send();
+		}else{
+			if(user.US_PASS==b.VIEJA)
+			{
+				//Actualizar la contraseña
+				collection.findByIdAndUpdate(
+					    user._id,
+					    $set: {US_PASS: b.NUEVA},
+					    {safe: true, upsert: true},
+					    function(err, model) {
+					        console.log(err);
+					    }
+					);
+			}
+				
+		}
+	});
 };
