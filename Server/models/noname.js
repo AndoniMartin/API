b5785a0e72ca82054a330970bc74887e94e155b8
andoni <andoni@phi-laptop>
2015-03-14 16:13:18 +0100

    Borrado los códigos de ejemplo.

--- Server/models/noname.js
+++
@@ -1,8 +0,0 @@
-exports = module.exports=function(app,mongoose)
-{
-	var modeloSchema = new mongoose.Schema({
-		//Aquí va la definición del esquema de mongoose
-		//ashdgadhgagdshsagdj
-		});
-	mongoose.model('Modelo',modeloSchema);
-};
