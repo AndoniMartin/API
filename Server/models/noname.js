f5e5435c2c3033fa1a5a50c6649ec434bb28bf02
Iñigo <Iigo@terminator>
2015-03-14 10:27:30 +0100

    ugfyvi

--- Server/models/noname.js
+++ Server/models/noname.js
@@ -2,6 +2,7 @@ exports = module.exports=function(app,mongoose)
 {
 	var modeloSchema = new mongoose.Schema({
 		//Aquí va la definición del esquema de mongoose
+		//ashdgadhgagdshsagdj
 		});
 	mongoose.model('Modelo',modeloSchema);
 };
