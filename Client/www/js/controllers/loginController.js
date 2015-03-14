4a21aa04a8adf385c37e11a45b370fd361ac0d8d
Iñigo <Iigo@terminator>
2015-03-14 12:26:51 +0100

    Coger valores del login, primer intento

--- Client/www/js/controllers/loginController.js
+++ Client/www/js/controllers/loginController.js
@@ -3,27 +3,42 @@ angular.module('APINotack.loginController', ['ionic',])
  .controller('loginCtrl', function ($scope,$location,$ionicPopup,restClient) {
 
         $scope.checkLogin=function(){
-        	var promise=restClient.login(usuario,contraseña);
-        	promise.then(function(logged){
-        		if(logged)
-        		{
-        			var user=new Object();
-        			user.name=null;
-        			user.pass=null;
-        			user.logged=logged;
-        			localStorage.setItem("user",JSON.stringify(user));
-        			$location.path("/notes");
-        		}
-        		else
-        		{
-        				   var alertPopup = $ionicPopup.alert({
-        				     title: 'Login incorrecto',
-        				     template: 'Usuario o contraseña incorrectos'
-        				   });
-        				   alertPopup.then(function(res) {
-        				     ;
-        				   });
-        		}
-        	})
+        	var user = $scope.login.user;
+        	var pass = $scope.login.pass;
+        	if(user!=null & pass!=null)
+        	{
+	        	var promise=restClient.login(user,pass);
+	        	promise.then(function(logged){
+	        		if(logged)
+	        		{
+	        			var user=new Object();
+	        			user.name=user;
+	        			user.pass=null;
+	        			user.logged=logged;
+	        			localStorage.setItem("user",JSON.stringify(user));
+	        			$location.path("/notes");
+	        		}
+	        		else
+	        		{
+	        				   var alertPopup = $ionicPopup.alert({
+	        				     title: 'Login incorrecto',
+	        				     template: 'Usuario o contraseña incorrectos'
+	        				   });
+	        				   alertPopup.then(function(res) {
+	        				     ;
+	        				   });
+	        		}
+	        	})
+        	}
+        	else
+       		{
+	        	var alertPopup = $ionicPopup.alert({
+				     title: 'Login vacío',
+				     template: 'Debe rellenar todos los campos.'
+				   });
+				alertPopup.then(function(res) {
+				     ;
+				   });
+       		}
         }
 })
