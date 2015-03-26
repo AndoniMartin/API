3968e096fc4d724b9e3f73ba95f10601a41de101
david <davidramirez_28@hotmail.com>
2015-03-26 17:59:35 +0100

    Cambios en el esquema de usuario y obtencion de notas en el servidor

--- Server/controllers/notesController.js
+++ Server/controllers/notesController.js
@@ -8,36 +8,37 @@ exports.addNote=function(request,response)
 	console.log(b);
 	
 	//Añadir la nota
-	var nota = new Note({ NO_Title: b.NO_Title, NO_Text: b.NO_Text, USER: b.US_Name}).save(function(error){
-		//Si se actualiza
-        if(error==null)
-        	{
-	        	//nota._id, id de la nota
-	        	
-	        	//El propietario tiene TY_ID de la nota = 1
-	        	var entrada = {
-	        			NO_ID: nota._id,
-	        			TY_ID: 1
-	        	}
-	        	
-	        	//Asociar la nota al usuario
-	        	User.findOneAndUpdate(
-	        		    {US_Name: b.US_Name},
-	        		    {$push: {Notes: entrada}},
-	        		    {safe: true, upsert: true},
-	        		    function(error, model) {
-	        		        console.log(error);
-	        		      //Si se actualiza
-	        		        if(error==null)
-	        		        	response.status(200).send(true);
-	        		        else
-	        		        	response.status(500).send();
-	        		    }
-	        		);
-        	}
-        else
-        	response.status(500).send();
+	Note.create({ NO_TITLE: b.NO_TITLE, NO_TEXT: b.NO_TEXT, USER: b.US_NAME}, function(err, nota){
+		if(err==null)
+		{
+			//nota._id, id de la nota
+			
+			//console.log(nota);
+			
+			//El propietario tiene TY_ID de la nota = 1
+			var entrada = {
+					NO_ID: nota._id,
+					TY_ID: 1
+			};
+			//console.log(entrada);
+			//Asociar la nota al usuario
+			User.update(
+				    {US_NAME: b.US_NAME},
+				    {$push: {NOTES: entrada}},
+				    function(error, model) {
+				    	console.log("tras actualizar "+model);
+				      //Si se actualiza
+				        if(error==null)
+				        	response.status(200).send(true);
+				        else
+				        	response.status(500).send();
+				    }
+				);
+		}
+		else
+			response.status(500).send();
 	});
+	//nota.save().then(function(note,error){console.log(note); console.log(error);})
 };
 
 
@@ -46,29 +47,65 @@ exports.getNotes=function(request,response)
 	var b=request.body;
 	console.log(b);
 	
-	User.findOne({US_NAME:b.US_NAME},function(error,user){
+	User.findOne({US_NAME:b.US_NAME}).populate('NOTES.NO_ID').exec(function(error, user){
 		if(error){
 			response.status(500).send();
 		}else{
-			if(user.US_PASS==b.US_PASS)
+			//Comprobar que el usuario existe
+			if(user == null)
 			{
-				//recorrer las notas del usuario y devolver el contenido de todas
-				
-				var notas = [];
-			
-				for(nota in user.NOTES){
-					notas.push(Note.find({NO_ID:nota.NO_ID}));
-				}
-				
-				//Devolver el array de notas en JSON
-				response.status(200).send(notas);
+				response.status(500).send();
 			}
 			else
 			{
-				response.status(200).send(false);
+				if(user.US_PASS==b.US_PASS)
+				{
+					//recorrer las notas del usuario y devolver el contenido de todas
+					
+					response.status(200).send(user.NOTES);
+					
+					//Devolver el array de notas en JSON
+					//response.status(200).send(notas);
+				}
+				else
+				{
+					response.status(200).send(false);
+				}
 			}
 		}
 	});
+	
+	/*User.findOne({US_NAME:b.US_NAME},function(error,user){
+		if(error){
+			response.status(500).send();
+		}else{
+			//Comprobar que el usuario existe
+			if(user == null)
+			{
+				response.status(500).send();
+			}
+			else
+			{
+				if(user.US_PASS==b.US_PASS)
+				{
+					//recorrer las notas del usuario y devolver el contenido de todas
+					
+					var notas = [];
+				
+					for(nota in user.NOTES){
+						notas.push(Note.findOne({NO_ID:nota.NO_ID}));
+					}
+					
+					//Devolver el array de notas en JSON
+					//response.status(200).send(notas);
+				}
+				else
+				{
+					response.status(200).send(false);
+				}
+			}
+		}
+	});*/
 };
 
 exports.updateNote=function(request,response)
--- Server/models/user.js
+++ Server/models/user.js
@@ -4,7 +4,7 @@ exports = module.exports=function(app,mongoose)
 	var userSchema = new Schema({
 		US_NAME:{type:String, required:true},
 		US_PASS:{type:String, required:true},
-		NO_ID:[{type:Schema.Types.ObjectId,ref:'note',required:true},{type:Number,required:true}]
+		NOTES:[{NO_ID:{type:Schema.Types.ObjectId,ref:'Note',required:true},TY_ID: {type:Number,required:true}}]
 		});
 	mongoose.model('User',userSchema);
 };
